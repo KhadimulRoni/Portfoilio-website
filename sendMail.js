43d9b5a7ff54e344faf8a9694ae6a463e99c7440
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-11-29 18:10:06 +0600

    send mail added

--- sendMail.js
+++ sendMail.js
@@ -1,1 +1,24 @@
-184A9BB5EF8379A106006E40ABEA3D6CB1E3
+window.onload = function () {
+   document
+      .getElementById('contact-form')
+      .addEventListener('submit', function (e) {
+         e.preventDefault();
+      });
+};
+function sendMail(params) {
+   var tempParams = {
+      from_name: document.getElementById('name').value,
+      from_email: document.getElementById('email').value,
+      from_subject: document.getElementById('subject').value,
+      message: document.getElementById('msg').value,
+   };
+
+   emailjs.send('gmail', 'template_dnht9rg', tempParams).then(
+      function () {
+         alert('CONGRATS! Message Successfully send');
+      },
+      function (error) {
+         alert('FAILED...', error);
+      }
+   );
+}
